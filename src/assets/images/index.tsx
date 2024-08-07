a2e6796eaeeeff24cd8db7fd6ee67b16aa0e22dc
vasuAI <vasu.choubey@appinventiv.com>
2024-08-07 18:07:47 +0530

    added assets folder.

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-unresolved */
+export default {
+  homeEnable: require('./icon/homeEnable.png'),
+  helpEnable: require('./icon/helpEnable.png'),
+  moreEnable: require('./icon/moreEnable.png'),
+  homeDisable: require('./icon/homeDisable.png'),
+  helpDiasble: require('./icon/helpDisable.png'),
+  moreDiasble: require('./icon/modeDisable.png'),
+};
