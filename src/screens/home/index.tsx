73a487056c634cb2c1c253502d46cbc6165ba1d7
vasuAI <vasu.choubey@appinventiv.com>
2024-09-18 12:17:26 +0530

    flex-box example.

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,6 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 import React from 'react';
-import styles from './styles';
+// import styles from './styles';
 import strings from '../../utils/string';
 import {ScreenNames} from '../../navigator/screenNames';
 import {useNavigation} from '@react-navigation/native';
@@ -9,27 +9,23 @@ const Home = () => {
   const navigation: any = useNavigation();
   const navigate = () => {
     // navigation.navigate(ScreenNames.BottomTab, {screen: ScreenNames.Profile});
-    navigation.navigate(
-      ScreenNames.BottomTab,
-      {screen: ScreenNames.Profile},
-      {params},
-    );
-    navigation.navigate(
-      ScreenNames.BottomTab,
-      {screen: ScreenNames.Profile},
-      params,
-    );
-    navigation.navigate(ScreenNames.BottomTab, {
-      screen: ScreenNames.Profile,
-      params,
-    });
+    navigation.navigate(ScreenNames.Profile);
   };
   return (
-    <View style={styles.root}>
-      <Text>{strings.hello}</Text>
-      <Button title="Go to Profile" onPress={navigate} />
+    <View style={styles.main}>
+      <View style={styles.red} />
+      <View style={styles.yellow} />
+
+      {/* <Text>{strings.hello}</Text>
+      <Button title="Go to Profile" onPress={navigate} /> */}
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  main: {flex: 1, backgroundColor: 'orange'},
+  red: {flex: 0.4, backgroundColor: 'red'},
+  yellow: {flex: 0.6, backgroundColor: 'yellow'},
+});
--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,20 +1,28 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet, SafeAreaView} from 'react-native';
 import React from 'react';
 import styles from '../home/styles';
 import {useNavigation} from '@react-navigation/native';
 import {ScreenNames} from '../../navigator/screenNames';
 
 const Profile = () => {
-  const navigation = useNavigation();
+  const navigation: any = useNavigation();
   const navigate = () => {
-    navigation.navigate(ScreenNames.Home);
+    navigation.navigate('Home');
   };
   return (
-    <View style={styles.root}>
-      <Text>Profile</Text>
-      <Button title="Go to Home" onPress={navigate} />
-    </View>
+    <SafeAreaView style={style.root}>
+      <View style={style.redBox} />
+      <View style={style.yellowBox} />
+      <View style={style.greenBox} />
+    </SafeAreaView>
   );
 };
 
 export default Profile;
+
+const style = StyleSheet.create({
+  root: {flex: 1, flexDirection: 'row', alignItems: 'center'},
+  redBox: {width: 50, height: 50, backgroundColor: 'red'},
+  yellowBox: {width: 50, height: 50, backgroundColor: 'yellow'},
+  greenBox: {width: 50, height: 50, backgroundColor: 'green'},
+});
